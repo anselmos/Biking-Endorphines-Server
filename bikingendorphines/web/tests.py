24c4b8e5614ce46e8d2084c61520e2ab8273e9aa
Anselmos <Github.com/Anselmos>
2017-03-26 21:10:29 +0200

    Initial GPXReader with Test-Cases

--- bikingendorphines/web/tests.py
+++ bikingendorphines/web/tests.py
@@ -1,11 +1,11 @@
 """
 Unit tests for web-application.
 """
-
-# disabling because this import will be used in future
-# pylint: disable=unused-import
 from django.test import TestCase
+import gpxpy
+
 from .models import User
+from .utils import GPXReader
 
 class UserTestCase(TestCase):
     """
@@ -24,3 +24,27 @@ class UserTestCase(TestCase):
         trab = User.objects.get(name='Trab')
         self.assertEqual(bart.bmi(), (80/(175*175)* 10000))
         self.assertEqual(trab.bmi(), (99/(175*175)* 10000))
+
+class GPXReaderTestCase(TestCase):
+    """
+    Tests all GPXReader class cases for methods access.
+    """
+
+    def setUp(self):
+
+        #pylint: disable=fixme
+        # TODO make mocking of example file!
+        self.gpxreader = GPXReader('bikingendorphines/example_data/15212277.gpx')
+
+    def test_get_points(self):
+        """
+        Tests if there will be data output from get_points.
+
+        Test if:
+        - points exists for different types.
+        - what will happen if types exists, but no data?
+        """
+        points = []
+        for point in self.gpxreader.get_points():
+            points.append(point)
+        self.assertGreater(len(points), 0)
--- bikingendorphines/web/utils.py
+++ bikingendorphines/web/utils.py
@@ -14,42 +14,71 @@ class GPXReader(object):
         Constructor. Reads default information that can be read by gpx.
         """
         self.file_handle = open(file_name)
+        #pylint: disable=fixme
         # FIXME Check if this is best idea to leave parsing in constructor - in scope of performance
-        self.gpx_handle = gpxpy.parse(self.file_handle)
+        # for now there is no better way I can find out.
+        # This will go to issues.
+        self.__gpx_handle = gpxpy.parse(self.file_handle)
+        self.__tracks = self.__gpx_handle.tracks
+        self.__routes = self.__gpx_handle.routes
+        self.__waypoints = self.__gpx_handle.waypoints
 
-        self.read_gpx_information()
-
-    # TODO change it to individual read!
-    def read_gpx_information(self):
+    def get_track_points(self, item_nb=0):
         """
-        Reads information from gpx file
+        Returns only first track points
         """
-        # 3 loops ... o2? o3?
-        for track in self.gpx_handle.tracks:
-            for segment in track.segments:
-                for point in segment.points:
-                    print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
+        points = []
+        if len(self.__tracks) > 0 and len(self.__tracks) >= item_nb:
+            for point in self.__tracks[item_nb].walk():
+                points.append(point)
+        return points
 
-        for waypoint in self.gpx_handle.waypoints:
-            print 'waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude)
-
-        for route in self.gpx_handle.routes:
-            print 'Route:'
-            for point in route.points:
-                print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
-                pass
-
-    def get_routes(self):
+    def get_route_points(self, item_nb=0):
         """
-        Returns list of routes with it's coordinates.
+        Returns route points
         """
-        return []
+        points = []
+        if len(self.__routes) > 0 and len(self.__routes) >= item_nb:
+            for point in self.__routes:
+                points.append(point)
+        return points
 
-    def get_elevation(self, route):
+    def get_way_points(self, item_nb=0):
+        """
+        Returns way points
+        """
+        points = []
+        if len(self.__waypoints) > 0 and len(self.__waypoints) >= item_nb:
+            for point in self.__waypoints:
+                points.append(point)
+        return points
+
+    def get_points(self, item_nb=0):
+        """
+        Returns Points from either route/track or waypoints.
+        If there is more then one type then uses Track's as a default.
+
+        Make assumption that there will be only one of each data_type
+        i.e. only one track/route/waypoint.
+        """
+        if len(self.__tracks) > 0 and self.__tracks[item_nb].get_points_no() > 0:
+            return self.get_track_points()
+        elif len(self.__routes) > 0 and self.__routes[item_nb].get_points_no() > 0:
+            return self.get_route_points()
+        elif len(self.__waypoints) > 0 and self.__waypoints[item_nb].get_points_no() > 0:
+            return self.get_way_points()
+        else:
+            return []
+
+    def get_elevations(self):
         """
         Returns list of elevation per route list
         """
-        return []
+        #pylint: disable=fixme
+        # TODO Check if this is not already handled somehow at gpxpy GPXTrackPoint!!
+        elevations = []
+        for point in self.get_points():
+            elevations.append(point.elevator())
 
 
 class EndomondoGPXReader(GPXReader):
@@ -58,13 +87,8 @@ class EndomondoGPXReader(GPXReader):
     GPX Reading class that uses gpxpy for reading stuff like:
     - routes in gpx file (amount)
     - lowest and highest points comparing to sea level (elevation)
-        # Use http://www.gpsvisualizer.com/elevation for 
+        # Use http://www.gpsvisualizer.com/elevation for
         # re-establishment of elevation if not properly assembled by android -application!
     - routes that looks like some animal ?? :D crazy ideas are the best!
     """
-
-    def __init__(self):
-        """
-        Constructor, will get information from mother.
-        """
-        pass
+    pass
