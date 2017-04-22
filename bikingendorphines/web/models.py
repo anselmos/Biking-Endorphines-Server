f88a65511e664264c0b4079fd3d4cb8e7ed3dca0
anselmos <anselmos@users.noreply.github.com>
2017-04-22 23:33:46 +0200

    Adds Test-Database models for REST-API schema.

--- bikingendorphines/web/models.py
+++ bikingendorphines/web/models.py
@@ -12,6 +12,7 @@ class User(models.Model):
     User model for obtaining personal information about biking riders
     """
     name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=100, default="")
     weight = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
 
@@ -26,3 +27,52 @@ class User(models.Model):
         Body Mass Index calculator simplified to number
         """
         return (self.weight / (self.height * self.height)) * 10000
+
+
+class Route(models.Model):
+    """
+    Route model. Defines Route by name, avg_route.
+    """
+    route_name = models.CharField(max_length=100)
+    avg_route = models.FloatField(default=0.0)
+
+    def __unicode__(self):
+        "Returns Route name "
+        return self.route_name
+
+
+class UserRoute(models.Model):
+    """
+    User's routes
+    """
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_route = models.ForeignKey(Route, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        "Returns User id"
+        return self.id_user
+
+
+class Point(models.Model):
+    """
+    Points similar to gpx-data
+    """
+    lat = models.FloatField(default=0.0)
+    lon = models.FloatField(default=0.0)
+    elevation = models.FloatField(default=0.0)
+
+    def __unicode__(self):
+        "Returns Point unicode"
+        return str(self.lat) + "" + str(self.lon)
+
+
+class RoutePoint(models.Model):
+    """
+    Route Points relations
+    """
+    id_route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    id_point = models.ForeignKey(Point, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        "Returns Route-Point relationship unicode"
+        return self.id_route
