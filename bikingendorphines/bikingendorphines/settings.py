c49ab6c9a01ddc492c32924d8561545a5fb8c254
anselmos <anselmos@users.noreply.github.com>
2017-04-28 13:47:42 +0200

    Adds STATIC_ROOT for fixing heroku collect-static.

--- bikingendorphines/bikingendorphines/settings.py
+++ bikingendorphines/bikingendorphines/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
