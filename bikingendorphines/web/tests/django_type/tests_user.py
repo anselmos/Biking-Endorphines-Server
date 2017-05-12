8461f39fdf8a695ddf8ea02645852bfe749129d9
anselmos <anselmos@users.noreply.github.com>
2017-05-12 08:12:45 +0200

    Adds fields as methods check for existing

--- bikingendorphines/web/tests/django_type/tests_user.py
+++ bikingendorphines/web/tests/django_type/tests_user.py
@@ -10,9 +10,14 @@ class UserModelTestCase(TestCase):
 
     def test_fields(self):
         """ testing if fields exists in model"""
-        fields = ['id', 'name', 'surname', 'weight', 'height']
+        fields = ['id', 'name', 'surname', 'weight', 'height', 'bmi']
         fields_in_model = []
+        # fields as django fields:
         for field in User._meta.get_fields(include_hidden=True):
             if field.name in fields:
                 fields_in_model.append(field.name)
+        # method as fields:
+        for method in User.__dict__:
+            if method in fields and method not in fields_in_model:
+                fields_in_model.append(method)
         assert sorted(set(fields_in_model).intersection(fields)) == sorted(fields)
