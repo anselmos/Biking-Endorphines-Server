bba2e3fd34d04a1d93ed8574b8243a9bf68166ca
anselmos <anselmos@users.noreply.github.com>
2017-05-12 08:27:42 +0200

    Refactors code
    Adds GenericModelTestCase and moves assert_fields into this class.
    UserModelTestCase inherits from GenericModelTestCase

--- bikingendorphines/web/tests/django_type/tests_user.py
+++ bikingendorphines/web/tests/django_type/tests_user.py
@@ -6,18 +6,27 @@ import logging
 
 from web.models import User
 
-class UserModelTestCase(TestCase):
+class GenericModelTestCase(TestCase):
+    cls = None
+    fields = []
 
-    def test_fields(self):
+    def assert_fields(self, expected):
         """ testing if fields exists in model"""
-        fields = ['id', 'name', 'surname', 'weight', 'height', 'bmi']
         fields_in_model = []
         # fields as django fields:
-        for field in User._meta.get_fields(include_hidden=True):
-            if field.name in fields:
+        for field in self.cls._meta.get_fields(include_hidden=True):
+            if field.name in expected:
                 fields_in_model.append(field.name)
         # method as fields:
-        for method in User.__dict__:
-            if method in fields and method not in fields_in_model:
+        for method in self.cls.__dict__:
+            if method in expected and method not in fields_in_model:
                 fields_in_model.append(method)
-        assert sorted(set(fields_in_model).intersection(fields)) == sorted(fields)
+        assert sorted(set(fields_in_model).intersection(expected)) == sorted(expected)
+
+
+class UserModelTestCase(GenericModelTestCase):
+    cls = User
+    fields = ['id', 'name', 'surname', 'weight', 'height', 'bmi']
+
+    def test_fields(self):
+        self.assert_fields(self.fields)
