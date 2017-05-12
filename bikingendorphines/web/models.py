0cf077457789dae9bb104e7816a0b15cc63f7d3a
anselmos <anselmos@users.noreply.github.com>
2017-05-12 19:26:05 +0200

    Refactors code. Removes useless pylint disable

--- bikingendorphines/web/models.py
+++ bikingendorphines/web/models.py
@@ -3,8 +3,6 @@ Models for web application
 """
 from __future__ import unicode_literals
 
-# disabling because this import will be used in future
-# pylint: disable=unused-import
 from django.db import models
 
 class User(models.Model):
