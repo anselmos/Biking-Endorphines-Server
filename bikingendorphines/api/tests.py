c8f2516bbb06f5579ea50f02f12ab066c8987e4f
anselmos <anselmos@users.noreply.github.com>
2017-05-13 18:00:21 +0200

    Adds test for creating user with API

--- bikingendorphines/api/tests.py
+++ bikingendorphines/api/tests.py
@@ -2,6 +2,7 @@
 Tests for API
 """
 import unittest
+import json
 from api.serializers import UserSerializer
 from api.views import UserList
 from web.models import User
@@ -48,14 +49,16 @@ class TestUserList(unittest.TestCase):
         self.token = Token.objects.get(user_id=self.user.id).key
 
     def tearDown(self):
+        "Removes all AuthUser and Users objects at the end of each test"
         self.user.delete()
+        User.objects.all().delete()
 
     def test_user_get_return_json(self):
         "test if using get returns json data"
 
         self.client.force_login(user=self.user)
         response = self.get_response_user_list()
-        self.assertEquals(response.status_code, 200)
+        self.assert_status_code(response)
 
     def test_user_list_return_json_list(self):
         " Test if setting up user returns user list"
@@ -64,22 +67,70 @@ class TestUserList(unittest.TestCase):
 
         self.client.force_login(user=self.user)
         response = self.get_response_user_list()
-        self.assertEquals(response.status_code, 200)
+        self.assert_status_code(response)
         self.assertEquals(len(response.data), 1)
         self.assert_user_object(response.data[0], input_user)
 
-    def assert_user_object(self, input, expected):
-        self.assertEquals(input['name'], expected.name)
-        self.assertEquals(input['surname'], expected.surname)
-        self.assertEquals(input['weight'], expected.weight)
-        self.assertEquals(input['height'], expected.height)
-        self.assertEquals(input['bmi'], expected.bmi())
-        self.assertEquals(input['bmi_health_name'], expected.bmi_health_name())
+    def test_get_two_user_list(self):
+        " Test if setting up user returns user list"
+
+        input_user = User.objects.create(name='Bart', surname="Trab", weight=80, height=175)
+        input_user2 = User.objects.create(name='B222art', surname="Trabaaaa", weight=50, height=100)
+
+        response = self.get_response_user_list()
+        self.assert_status_code(response)
+        self.assertEquals(len(response.data), 2)
+        self.assert_user_object(response.data[0], input_user)
+        self.assert_user_object(response.data[1], input_user2)
+
+    def assert_status_code(self, response):
+        " Asserts response status code with 200"
+        self.assertEquals(response.status_code, 200)
+
+    def assert_user_object(self, input_json, expected):
+        "Asserts response User input json object == expected user model object. "
+        self.assertEquals(input_json['name'], expected.name)
+        self.assertEquals(input_json['surname'], expected.surname)
+        self.assertEquals(input_json['weight'], expected.weight)
+        self.assertEquals(input_json['height'], expected.height)
+        self.assertEquals(input_json['bmi'], expected.bmi())
+        self.assertEquals(input_json['bmi_health_name'], expected.bmi_health_name())
 
     def get_response_user_list(self):
+        " Creates response for /api/user List get with forcing login with Token-Authentication "
+        self.client.force_login(user=self.user)
         view = UserList.as_view()
         factory = APIRequestFactory()
         request = factory.get("/api/user/", HTTP_AUTHORIZATION='Token {}'.format(self.token))
         force_authenticate(request)
         response = view(request)
         return response
+
+class TestUser(unittest.TestCase):
+    "UserList tests"
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = AuthUser.objects.create_superuser('admin', 'admin@admin.com', 'admin123')
+        self.token = Token.objects.get(user_id=self.user.id).key
+
+    def tearDown(self):
+        "Removes all AuthUser and Users objects at the end of each test"
+        self.user.delete()
+        User.objects.all().delete()
+
+    def test_return_user_object(self):
+        " Tests if making api request returns user object "
+
+        view = UserList.as_view()
+        factory = APIRequestFactory()
+        request = factory.post(
+            "/api/user/",
+            json.dumps(
+                {"name":"Test", "surname":"tester1", "weight":88, "height":173}
+            ), HTTP_AUTHORIZATION='Token {}'.format(self.token),
+            content_type='application/json'
+        )
+        force_authenticate(request)
+        response = view(request)
+        self.assertEquals(response.status_code, 201)
--- bikingendorphines/bikingendorphines/settings.py
+++ bikingendorphines/bikingendorphines/settings.py
@@ -53,14 +53,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.TokenAuthentication',
-#     )
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 ROOT_URLCONF = 'bikingendorphines.urls'
 
