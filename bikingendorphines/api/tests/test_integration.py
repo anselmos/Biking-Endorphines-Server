cd128b1c59ee8662bfac12d12f022d352f8347ae
anselmos <anselmos@users.noreply.github.com>
2017-05-15 12:06:38 +0200

    Moves tests from file to module-based and into proper file-names(unit/integration)

--- bikingendorphines/api/tests/test_integration.py
+++ bikingendorphines/api/tests/test_integration.py
@@ -1,5 +1,5 @@
 """
-Tests for API
+Tests Integration for API
 """
 import unittest
 import json
@@ -51,32 +51,6 @@ class APIGeneralTestCase(unittest.TestCase):
     def create_user_in_db(self):
         " Creates new user object in database "
         return User.objects.create(name='Bart', surname="Trab", weight=80, height=175)
-
-class TestUserSerializer(unittest.TestCase):
-    "UserSerializer tests"
-
-    def test_user_serialize_to_json(self):
-        "test if serializing to JSON works"
-
-        mocked = User()
-        mocked.name = "Anselmos"
-        mocked.surname = "Somlesna"
-        mocked.weight = 80
-        mocked.height = 175
-
-        user_serialized = UserSerializer(mocked)
-        self.assertEqual(
-            (user_serialized.data),
-            {
-                'height': 175,
-                'surname': u'Somlesna',
-                'id': None,
-                'weight': 80,
-                'name': u'Anselmos',
-                'bmi': 26,
-                'bmi_health_name': u'Overweight'
-            }
-        )
 
 
 class TestUserList(APIGeneralTestCase):
---
+++ bikingendorphines/api/tests/test_unit.py
@@ -0,0 +1,33 @@
+"""
+All unit-type tests
+For now Serializer's tests coexists here
+"""
+import unittest
+from web.models import User
+from api.serializers import UserSerializer
+
+class TestUserSerializer(unittest.TestCase):
+    "UserSerializer tests"
+
+    def test_user_serialize_to_json(self):
+        "test if serializing to JSON works"
+
+        mocked = User()
+        mocked.name = "Anselmos"
+        mocked.surname = "Somlesna"
+        mocked.weight = 80
+        mocked.height = 175
+
+        user_serialized = UserSerializer(mocked)
+        self.assertEqual(
+            (user_serialized.data),
+            {
+                'height': 175,
+                'surname': u'Somlesna',
+                'id': None,
+                'weight': 80,
+                'name': u'Anselmos',
+                'bmi': 26,
+                'bmi_health_name': u'Overweight'
+            }
+        )
